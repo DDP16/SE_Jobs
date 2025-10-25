d92c3cc1e74ea951cdda8f24fff968d2e3cacbc6
DDP16 <phat16122004@gmail.com>
2025-10-25 15:21:16 +0700

    clean architecture 25-10-2025

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import './App.css'
 import './app/styles/colors.css'
 import './app/styles/font.css'
 import './settings/i18n'
-import { useTranslation } from 'react-i18next'
-import ThemeProvider from './app/providers/ThemeProvider'
-import MainLayout from './app/layouts/MainLayout'
-import Home from './app/pages/Home'
 import MainRoutes from './app/routes/MainRoutes'
 import './app/styles/colors.css'
 import './app/styles/font.css'
-import './i18n'
 
 function App() {
   return <MainRoutes />
