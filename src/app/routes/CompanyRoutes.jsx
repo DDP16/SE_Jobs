3c6b17d7f91a8248f7211b3985645d12d68bcb5d
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2025-11-30 11:05:16 +0700

    feat: add company post job screen

--- src/app/routes/CompanyRoutes.jsx
+++ src/app/routes/CompanyRoutes.jsx
@@ -1,25 +1,28 @@
+import { Routes, Route, Outlet } from "react-router-dom";
 import CompanyLayout from "@/layouts/CompanyLayout";
 import PageNotFound from "@/layouts/PageNotFound";
-import {
-    ApplicantDetails,
-    CompanyProfile,
-    CompanyDashboard as Dashboard,
-} from "@/pages";
-import { Outlet, Route, Routes } from "react-router-dom";
+import CompanyProfile from "@/pages/Company/CompanyProfile";
+import ApplicantDetails from "@/pages/Company/ApplicantDetails";
+import PostJob from "@/pages/Company/PostJob/PostJob";
+import Dashboard from "../pages/Company/DashBoard/Dashboard";
 
 export default function CompanyRoutes() {
-    return (
-        <Routes>
-            <Route path="/" element={
-                <CompanyLayout>
-                    <Outlet />
-                </CompanyLayout>
-            }>
-                <Route index element={<Dashboard />} />
-                <Route path="company" element={<CompanyProfile />} />
-                <Route path="applicants/:id" element={<ApplicantDetails />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route
+        path=""
+        element={
+          <CompanyLayout>
+            <Outlet />
+          </CompanyLayout>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="postjob" element={<PostJob />} />
+        <Route path="company" element={<CompanyProfile />} />
+        <Route path="applicants/:id" element={<ApplicantDetails />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
+  );
 }
--- src/app/routes/MainRoutes.jsx
+++ src/app/routes/MainRoutes.jsx
@@ -17,6 +17,7 @@ export default function MainRoutes() {
         ) : (
           <UserRoutes />
         )}
+        {/* <CompanyRoutes /> */}
       </BrowserRouter>
     </ThemeProvider>
   );
